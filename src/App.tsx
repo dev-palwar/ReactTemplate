f09b47f8ac9c494ef205679290fde6ed418234d7
dev <devpalwar06@gmail.com>
2023-12-28 12:13:39 +0530

    It's a template now

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,7 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-import Login from "./Components/Login";
-import Home from "./Home";
 
 const App: React.FC = () => {
-  const isUserLoggedIn = !!localStorage.getItem("userDetails");
-
-  const HomeRoute = () => {
-    if (isUserLoggedIn) {
-      return <Home />;
-    } else {
-      return <Navigate to="/login" />;
-    }
-  };
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<HomeRoute />} />
-        <Route index element={<Navigate to="/home" />} />
-      </Routes>
-    </Router>
-  );
+  return <div className="text-red-700">Hello template</div>;
 };
 
 export default App;
